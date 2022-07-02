41c233fb8eb10776a5e406d5c33d75524c4f7c18
JC Wang <wang_junchuan@163.com>
2022-07-02 18:12:54 +0800

    Deployed 18634d9 with MkDocs version: 1.3.0

---
+++ assets/javascripts/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ca318366093f47c338fc32c3f9f97c32";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
